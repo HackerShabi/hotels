45614ca606d3d3a278a4103673b71c782beb5263
HackerShabi <hackershabi@gmail.com>
2025-06-28 23:43:41 +0500

    Setup Firebase GitHub auto-deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
-      ✅ Tailwind + React working!
+      ✅ Tailwind + React working! fbrtsngrenwbggrjebhwjrbvhjewbvjhebvjhe vjefbvjew
     </div>
-  );
+      );
 }
 
 export default App;
