5da11247946e6c466f4328ef37a60c54424cb0ae
HackerShabi <hackershabi@gmail.com>
2025-06-29 00:07:21 +0500

    🚀 Test auto-deploy from GitHub

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
-      ✅ Tailwind + React working! fbrtsngrenwbggrjebhwjrbvhjewbvjhebvjhe vjefbvjew
+      ✅ Tailwind  shabj sbbhbhjjbeddhjbej + React working! fbrtsngrenwbggrjebhwjrbvhjewbvjhebvjhe vjefbvjew
     </div>
       );
 }
